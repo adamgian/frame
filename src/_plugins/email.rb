0af8adee318fd8b14bb375c7c0144aff0e20f5b1
Adam Gian <contact@adamgian.com>
2023-04-08 22:34:56 +1000

    refator: font imports

--- src/_plugins/email.rb
+++ src/_plugins/email.rb
@@ -1,48 +1,48 @@
 module Jekyll
 
-    class EmailTag < Liquid::Tag
+	class EmailTag < Liquid::Tag
 
-        def initialize(tag_name, text, tokens)
-            super
-            @email = text.strip
-        end
+		def initialize(tag_name, text, tokens)
+			super
+			@email = text.strip
+		end
 
-        def render(context)
-            email_link = obfuscate_email_link(@email)
-            email_display = obfuscate_email_display(@email)
-            "<a class=\"anchor\" href=\"#{email_link}\">#{email_display}</a>"
-        end
+		def render(context)
+			email_link = obfuscate_email_link(@email)
+			email_display = obfuscate_email_display(@email)
+			"<a class=\"anchor\" href=\"#{email_link}\">#{email_display}</a>"
+		end
 
-        private
-        def obfuscate_email_link(string)
-            string = "mailto:#{string}".chars
-            string.map!.with_index { |char, index|
-                if char.match(/^[:@.]/i)
-                    char = "&#x#{char.ord.to_s(16)};"
-                elsif char.match(/^[aeiou]/i) && index > 7
-                    char = "%#{char.ord.to_s(16)}"
-                else
-                    char = "&##{char.ord.to_s};"
-                end
-            }
-            return string.join('')
-        end
+		private
+		def obfuscate_email_link(string)
+			string = "mailto:#{string}".chars
+			string.map!.with_index { |char, index|
+				if char.match(/^[:@.]/i)
+					char = "&#x#{char.ord.to_s(16)};"
+				elsif char.match(/^[aeiou]/i) && index > 7
+					char = "%#{char.ord.to_s(16)}"
+				else
+					char = "&##{char.ord.to_s};"
+				end
+			}
+			return string.join('')
+		end
 
-        private
-        def obfuscate_email_display(string)
-            string = string.chars
-            string.map! { |char|
-                if char.match(/^[@.]/i)
-                    char = "<i>&#x#{char.ord.to_s(16)};</i>"
-                    char += '<!-- @ -->'
-                else
-                    char = "&##{char.ord.to_s};"
-                end
-            }
-            return string.join('')
-        end
+		private
+		def obfuscate_email_display(string)
+			string = string.chars
+			string.map! { |char|
+				if char.match(/^[@.]/i)
+					char = "<i>&#x#{char.ord.to_s(16)};</i>"
+					char += '<!-- @ -->'
+				else
+					char = "&##{char.ord.to_s};"
+				end
+			}
+			return string.join('')
+		end
 
-    end
+	end
 
 end
 
